37fe863dca981d325591e58a247c75a23f56fc69
Joao Nakano <joaovynak@gmail.com>
2024-02-08 08:27:38 -0300

    javascript output code addition

---
+++ W3Schools/javascript-output/main.js
@@ -0,0 +1,2 @@
+document.getElementById("demo").innerHTML = "A Custom paragraph made with JavaScript"; // Most common way of displaying data in HTML
+document.write("Hello, world!") // Should be used only for testing
